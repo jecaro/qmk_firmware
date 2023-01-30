424aef87d391efbe62ae39ef9018705473a945a7
jecaro <jeancharles.quillet@gmail.com>
2023-01-30 18:01:35 +0100

    lower the tapping term

--- keyboards/sofle/keymaps/jecaro/config.h
+++ keyboards/sofle/keymaps/jecaro/config.h
@@ -5,7 +5,7 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 #undef TAPPING_TERM
-#define TAPPING_TERM 250
+#define TAPPING_TERM 200
 
 #undef ENCODER_RESOLUTION
 #define ENCODER_RESOLUTION 4
