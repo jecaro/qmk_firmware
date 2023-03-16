c65c1a8ede4d35d1a7bf436b29c2caeb80a3852e
jecaro <jeancharles.quillet@gmail.com>
2023-03-16 10:18:52 +0100

    replace tapping term by IGNORE_MOD_TAP_INTERRUPT

--- keyboards/sofle/keymaps/jecaro/config.h
+++ keyboards/sofle/keymaps/jecaro/config.h
@@ -5,7 +5,7 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 #undef TAPPING_TERM
-#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #undef ENCODER_RESOLUTION
 #define ENCODER_RESOLUTION 4
