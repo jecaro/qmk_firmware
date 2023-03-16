3368278230de5997497c836da4ed7637eac974a4
jecaro <jeancharles.quillet@gmail.com>
2023-03-16 10:19:38 +0100

    remove OLED support

--- keyboards/sofle/keymaps/jecaro/keymap.c
+++ keyboards/sofle/keymaps/jecaro/keymap.c
@@ -19,11 +19,6 @@ enum custom_keycodes {
 #define SYM(_x_) LT(_SYMBOLS, _x_)
 #define NUM(_x_) LT(_NUMBERS, _x_)
 
-// TODO: add combo
-// ctrl + alt + del
-// ctrl + alt + f1-f12
-// show modifiers on oled
-
 // layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -34,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   A  |   Z  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  ^ " |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   Q  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   M  |  ù % |
- * |------+------+------+------+------+------| MUTE  |    | PLAY  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| PLAY  |    | MUTE  |------+------+------+------+------+------|
  * |LShift|   W  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |  , ? |  ; . |  : / |  ! § |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCtr | LGUI | LAlt |ESC   | /Space  /       \Enter \  |Bspc  | RAlt | RGUI | RCtr |
@@ -128,61 +123,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   return true;
 }
-
-#ifdef OLED_ENABLE
-
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
-static void print_status_narrow(void) {
-    // Print current mode
-    oled_write_P(PSTR("\n\n"), false);
-    oled_write_ln_P(PSTR("MODE"), false);
-    oled_write_ln_P(PSTR(""), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case _AZERTY:
-            oled_write_ln_P(PSTR("Base"), false);
-            break;
-        case _NAVIGATION:
-            oled_write_ln_P(PSTR("Nav"), false);
-            break;
-        case _SYMBOLS:
-            oled_write_ln_P(PSTR("Sym"), false);
-            break;
-        case _NUMBERS:
-            oled_write_ln_P(PSTR("Num"), false);
-            break;
-        default:
-            oled_write_P(PSTR("Undef"), false);
-    }
-    oled_write_P(PSTR("\n\n"), false);
-}
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
-}
-
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        print_status_narrow();
-    } else {
-        render_logo();
-    }
-    return false;
-}
-
-#endif
 
 #ifdef ENCODER_ENABLE
 
