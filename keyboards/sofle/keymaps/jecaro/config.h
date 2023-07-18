6641a9767a160ee9e32eef5353a7487ebf2c0e13
jecaro <jeancharles.quillet@gmail.com>
2023-07-18 16:55:15 +0200

    update my keyboards following the merge

--- keyboards/sofle/keymaps/jecaro/config.h
+++ keyboards/sofle/keymaps/jecaro/config.h
@@ -5,7 +5,6 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 #undef TAPPING_TERM
-#define IGNORE_MOD_TAP_INTERRUPT
 
 #undef ENCODER_RESOLUTION
 #define ENCODER_RESOLUTION 4
