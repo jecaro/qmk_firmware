50040c6215b221ff06ddc434897724119af89a29
jecaro <jeancharles.quillet@gmail.com>
2023-07-18 15:42:20 +0200

    add sofle choc keyboard

---
+++ keyboards/sofle/keymaps/jecaro_choc_lp/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#undef TAPPING_TERM
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#undef ENCODER_RESOLUTION
+#define ENCODER_RESOLUTION 2
---
+++ keyboards/sofle/keymaps/jecaro_choc_lp/rules.mk
@@ -0,0 +1,1 @@
+../jecaro/rules.mk
